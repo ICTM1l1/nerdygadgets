a2efdc74f5b143b89fd963b3831f0a64af57bd93
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2020-11-11 19:00:35 +0100

    Add 404 page

---
+++ Errors/404.php
@@ -0,0 +1,16 @@
+<?php
+require_once __DIR__ . "/../Src/header.php";
+?>
+<div class="row">
+    <div class="col-sm-12 mt-2 text-center">
+        <h1>Deze pagina bestaat niet.</h1>
+    </div>
+
+    <div class="col-sm-12 mt-2 text-center">
+        <a href="../index.php">Ga terug naar de homepagina.</a>
+    </div>
+</div>
+<?php
+require_once __DIR__ . "/../Src/footer.php";
+?>
+
