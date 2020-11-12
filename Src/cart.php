d9a5a7509ab1f8dbb2a7207cce28f134708d8fca
ikbenlike <thewatcheriswatchinu@gmail.com>
2020-11-12 15:50:39 +0100

    start implementing cart backend

---
+++ Src/cart.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+
+class CartItem {
+    private $code;
+    private $count;
+
+    function __construct($code, $count) {
+        $this->code = $code;
+        $this->count = $count;
+    }
+
+    function get_code(){
+        return $this->code;
+    }
+
+    function get_count(){
+        return $this->count;
+    }
+
+    function set_count($count){
+        $this->count = $count;
+    }
+}
+
+class Cart {
+    private $items;
+
+    function __construct(){
+        $this->items = array();
+    }
+
+    function get_item($n){
+        return $this->items[$n];
+    }
+
+    function get_items(){
+        return $this->items;
+    }
+
+    function add_item($code, $count){
+        $this->items[] = CartItem($code, $count);
+    }
+
+    function remove_item($n){
+        array_splice($this->items, $n, 1);
+    }
+}
