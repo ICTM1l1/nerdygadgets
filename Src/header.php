3e38145ee5aba72cf29d974b66e76f0d09ecf28d
ikbenlike <thewatcheriswatchinu@gmail.com>
2020-11-17 16:39:24 +0100

    switch load order and session init to prevent PHP incomplete object errors

--- Src/header.php
+++ Src/header.php
@@ -1,12 +1,9 @@
 <?php
-if(session_status() == PHP_SESSION_NONE){
-    session_start();
-}
-
 require_once __DIR__ . '/../Src/Core/core.php';
 require_once __DIR__ . '/../Src/Crud/crud.php';
 
-if(!isset($_SESSION["cart"])){
+if(session_status() == PHP_SESSION_NONE){
+    session_start();
     $_SESSION["cart"] = new Cart();
 }
 
