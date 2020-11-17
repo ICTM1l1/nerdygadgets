8721c91e018940193186644917fbc2ddb8f8c6b8
Jeffrey Kosse <j3fftw@gmail.com>
2020-11-17 15:23:20 +0100

    Theme the shopping cart view

--- Config/config.php
+++ Config/config.php
@@ -8,7 +8,7 @@ $config['database_server'] = 'mysql:host=localhost';
 $config['database_name'] = 'nerdygadgets';
 $config['database_user'] = 'root';
 $config['database_password'] = '';
-$config['database_port'] = 3306;
+$config['database_port'] = 4444;
 $config['database_charset'] = 'utf8';
 
 // If we are on the production site, turn this off.
