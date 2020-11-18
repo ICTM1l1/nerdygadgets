4bcb057806dbebab063b7527e545acad2d82b4b2
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2020-11-18 14:07:28 +0100

    Theme the form progress bar

--- Public/checkout.php
+++ Public/checkout.php
@@ -13,28 +13,7 @@ if (empty($price) || empty($cart->getItems())) {
 ?>
     <div class="container-fluid">
         <div class="products-overview w-50 ml-auto mr-auto mt-5 mb-5">
-            <div class="row">
-                <div class="col-sm-12">
-                    <h1 class="mb-5 float-left">1. Bezorggegevens</h1>
-
-                    <div class="form-progress float-right">
-                        <!-- Grey with black text -->
-                        <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
-                            <ul class="navbar-nav">
-                                <li class="nav-item active border-right border-white">
-                                    <a class="nav-link" href="#">Bezorggegevens</a>
-                                </li>
-                                <li class="nav-item border-right border-white">
-                                    <a class="nav-link" href="#">Afrekenen</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Afronden</a>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-            </div>
+            <?php include_once __DIR__ . '/Html/form-progress.php'; ?>
 
             <div class="row">
                 <div class="col-sm-12">
--- Public/transactioncomplete.php
+++ Public/transactioncomplete.php
@@ -63,28 +63,7 @@ if ($paymentPaid) {
 
 <div class="container-fluid">
     <div class="products-overview w-50 ml-auto mr-auto mt-5 mb-5">
-        <div class="row">
-            <div class="col-sm-12">
-                <h1 class="mb-5 float-left">3. Afronden</h1>
-
-                <div class="form-progress float-right">
-                    <!-- Grey with black text -->
-                    <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
-                        <ul class="navbar-nav">
-                            <li class="nav-item border-right border-white">
-                                <a class="nav-link" href="#">Bezorggegevens</a>
-                            </li>
-                            <li class="nav-item border-right border-white">
-                                <a class="nav-link" href="#">Afrekenen</a>
-                            </li>
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Afronden</a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </div>
+        <?php include_once __DIR__ . '/Html/form-progress.php'; ?>
 
         <div class="row">
             <div class="col-sm-12">
