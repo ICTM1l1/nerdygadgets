6b7e4fef34953a51b9bfaa38878d9575d448bc2b
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2020-11-18 15:46:01 +0100

    Refactor alert to a reusable piece of html

--- Src/header.php
+++ Src/header.php
@@ -72,16 +72,4 @@ $categories = getCategories();
         <div class="col-12">
             <div id="SubContent">
 
-                <?php
-                    $errors = get_user_errors();
-                    $message = '';
-                    foreach ($errors as $error) {
-                        $message .= "{$error} <br>";
-                    }
-
-                    if ($message !== '') : ?>
-                    <div class="alert alert-danger w-50 ml-auto mr-auto mt-2 mb-2" role="alert">
-                        <?= $message ?>
-                    </div>
-                <?php endif; ?>
-
+                <?php include __DIR__ . '/Html/alert.php'; ?>
