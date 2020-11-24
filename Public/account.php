ed5e18009d31f9d3d5ddb4f6b52c6fc847223a3c
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2020-11-24 16:41:55 +0100

    Make the account page wider

--- Public/account.php
+++ Public/account.php
@@ -3,7 +3,7 @@ require_once __DIR__ . "/../Src/header.php";
 ?>
 
     <div class="container-fluid">
-        <div class="w-50 mt-1 ml-auto mr-auto">
+        <div class="w-75 mt-1 ml-auto mr-auto">
             <?php include_once __DIR__ . '/../Src/Html/account-navbar.php'; ?>
 
             <div class="account-overview mt-3 mb-5">
