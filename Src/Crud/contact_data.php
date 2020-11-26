bc73fc5998a19e1ae8a78b518225c3848db58a9f
ikbenlike <thewatcheriswatchinu@gmail.com>
2020-11-26 15:27:50 +0100

    add database functions for manipulating contact requests

--- Src/Crud/contact_data.php
+++ Src/Crud/contact_data.php
@@ -1,1 +1,53 @@
 <?php
+/**
+ * Create a contact request.
+ *
+ * @param string $name
+ *   Name of the person filing the reauest.
+ * @param string $email
+ *   Email address of the person filing the request.
+ * @param string $subject
+ *   The subject the contact request pertains to.
+ * @param string $message
+ *   The message body text.
+ *
+ * @return int
+ *   The contact request ID.
+ */
+function createContactRequest(string $name, string $email, string $subject, string $message){
+    return insert("contact_requests", [
+        "ContactRequestName" => $name,
+        "ContactRequestEmail" => $email,
+        "ContactRequestSubject" => $subject,
+        "ContactRequestMessage" => $message
+    ]);
+}
+
+/**
+ * Retrieve contact request by ID.
+ *
+ * @param int $id
+ *   The ID of the contact request that is to be retrieved.
+ *
+ * @return array
+ *   The retrieved contact request.*/
+function getContactRequestByID(int $id){
+    return selectFirst(
+        "SELECT * FROM contact_requests
+                WHERE ContactRequestID = :id",
+        ["id" => $id]
+    );
+}
+
+
+/**
+ * Remove contact request based on ID.
+ *
+ * @param int $id
+ *   The ID of the contact request that is to be removed.
+ *
+ * @return int
+ *   The result of the query.*/
+function removeContactRequest(int $id){
+    return delete("contact_requests", ["ContactRequestID" => $id]);
+}
--- Src/Crud/crud.php
+++ Src/Crud/crud.php
@@ -4,3 +4,4 @@ require_once __DIR__ . '/category.php';
 require_once __DIR__ . '/product.php';
 require_once __DIR__ . '/customer.php';
 require_once __DIR__ . '/city.php';
+require_once __DIR__ . '/contact_data.php';
