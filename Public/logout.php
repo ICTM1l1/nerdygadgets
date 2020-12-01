6e85be00b8d883dc50bae78672e16c3e1190e2f2
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2020-12-01 13:03:50 +0100

    Restart the session on logout

--- Public/logout.php
+++ Public/logout.php
@@ -1,11 +1,10 @@
 <?php
 require_once __DIR__ . "/../src/header.php";
 
-session_key_unset('LoggedIn');
-session_key_unset('personID');
+session_destroy();
+session_start();
 
 add_user_message('Je bent succesvol uitgelogd.');
 redirect('login.php');
 
 require_once __DIR__ . "/../src/footer.php";
-?>
