573fd62008104755b4dea608d2ddc5361f71ff74
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2020-12-01 11:53:52 +0100

    Debug contact requests overview styling

--- Public/contact-requests.php
+++ Public/contact-requests.php
@@ -67,7 +67,7 @@ if (isset($_POST['delete_contact_request'])) {
                                 <?php endif; ?>
 
                                 <?php if ($amountContactRequests > 0) : ?>
-                                    <div class="col-sm-3 pt-0 mt-0">
+                                    <div class="col-sm-4 pt-0 mt-0">
                                         <div class="form-label-group">
                                             <label for="searchListGroupItems" class="d-none">
                                                 <b>Zoeken</b>
@@ -99,7 +99,7 @@ if (isset($_POST['delete_contact_request'])) {
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-sm-9">
+                                    <div class="col-sm-8">
                                         <div class="tab-content text-white" id="nav-tabContent">
                                             <?php $active = 'active';
                                             foreach ($contactRequests as $key => $contactRequest) :
