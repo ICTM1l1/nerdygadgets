4463eb2a89b2b20253b21d8bcd13dd20716e27e5
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2020-12-09 08:39:54 +0100

    Add security measures

--- Src/header.php
+++ Src/header.php
@@ -4,6 +4,12 @@ ob_start();
 require_once __DIR__ . '/../Src/Core/core.php';
 require_once __DIR__ . '/../Src/Crud/crud.php';
 
+// Server should keep session data for AT LEAST 1 hour.
+ini_set('session.gc_maxlifetime', 3600);
+
+// Each client should remember their session id for EXACTLY 1 hour.
+session_set_cookie_params(3600);
+
 securityHeaders();
 session_start();
 secureSession();
