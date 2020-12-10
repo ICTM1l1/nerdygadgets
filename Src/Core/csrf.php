5a20329ac4f1898dd12d24f839f871c620ab7880
ikbenlike <thewatcheriswatchinu@gmail.com>
2020-12-10 14:08:13 +0100

    add documentation, sensible defaults

--- Src/Core/csrf.php
+++ Src/Core/csrf.php
@@ -1,8 +1,29 @@
 <?php
-function csrf_token_private($size=32){
+
+/**
+ * Generate a semi-random private token to use as key for CSRF token.
+ *
+ * @param int $size
+ *   Token length in bytes.
+ * @return string
+ *   String representation of the generated token.
+ * @throws Exception
+ *   Exception is thrown when no randomness source can be found.
+ */
+function csrf_token_private(int $size=32){
     return bin2hex(random_bytes($size));
 }
 
+/**
+ * Generate CSRF token from current page and pseudo-random key.
+ *
+ * @return string
+ *   Returns string representation of computed token. Empty string if
+ *   something has gone wrong.
+ * @throws Exception
+ *   Thrown when there is no adequate randomness source for the
+ *   pseudo-random token.
+ */
 function csrf_token(){
     if(session_status() != PHP_SESSION_ACTIVE){
         return false;
@@ -15,10 +36,25 @@ function csrf_token(){
     }
     session_save("ptoken", csrf_token_private(), $overwrite);
     session_save("pexpiry", time() + 3600, $overwrite);
-    return hash_hmac("sha256", $_SERVER["SCRIPT_NAME"], $_SESSION["ptoken"]);
+    return hash_hmac("sha256", $_SERVER["SCRIPT_NAME"], $_SESSION["ptoken"]) ?? "";
 }
 
-function csrf_validate($token, $destination){
+/**
+ * Check if the sent token matches the presently valid one.
+ * Redirects and adds user error if token not valid.
+ *
+ * @param string $token
+ *   String representation of the received token that is to be checked.
+ * @param string $destination
+ *   Page to redirect to if tokens do not match. Does not redirect on
+ *   empty string.
+ * @return bool
+ *   Returns true of tokens match. Only returns false when there is
+ *   no redirect (empty redirect string).
+ * @throws Exception
+ *   Thrown when there is no adequate randomness source for private key.
+ */
+function csrf_validate($token, $destination = ''){
     if(hash_equals(csrf_token(), $token)){
         return true;
     }
