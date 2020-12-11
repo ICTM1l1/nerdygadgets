0f516d07901a143331b9ced97a35901630e8b81a
ikbenlike <thewatcheriswatchinu@gmail.com>
2020-12-11 03:35:24 +0100

    add review.php

--- Src/Crud/crud.php
+++ Src/Crud/crud.php
@@ -7,3 +7,4 @@ require_once __DIR__ . '/city.php';
 require_once __DIR__ . '/contact_data.php';
 require_once __DIR__ . '/order.php';
 require_once __DIR__ . '/people.php';
+require_once __DIR__ . '/review.php';
--- Src/Crud/review.php
+++ Src/Crud/review.php
@@ -1,1 +1,8 @@
 <?php
+function createReview(int $sid, int $pid, string $review){
+    return insert("review", [
+        "StockItemID" => $sid,
+        "Review" => $review,
+        "PrivateCustomerID" => $pid
+    ]);
+}
