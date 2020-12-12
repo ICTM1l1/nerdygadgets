187b6a8cb0e8d9efaf892149a266727a69611404
mickd <worldofwarcraftmick@hotmail.nl>
2020-12-12 13:46:48 +0100

    Hotfix database improvements

--- database-improvements.sql
+++ database-improvements.sql
@@ -3,6 +3,7 @@
  */
 SET FOREIGN_KEY_CHECKS = 0;
 
+LOCK TABLES `people` WRITE;
 ALTER TABLE `people`
 CHANGE COLUMN `PersonID` `PersonID` INT(11) NOT NULL AUTO_INCREMENT;
 
@@ -19,85 +20,112 @@ LOCK TABLES `orderlines` WRITE;
 ALTER TABLE `orderlines`
 CHANGE COLUMN `OrderLineID` `OrderLineID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `buyinggroups` WRITE;
 ALTER TABLE `buyinggroups`
 CHANGE COLUMN `BuyingGroupID` `BuyingGroupID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `cities` WRITE;
 ALTER TABLE `cities`
 CHANGE COLUMN `CityID` `CityID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `colors` WRITE;
 ALTER TABLE `colors`
 CHANGE COLUMN `ColorID` `ColorID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `countries` WRITE;
 ALTER TABLE `countries`
 CHANGE COLUMN `CountryID` `CountryID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `customercategories` WRITE;
 ALTER TABLE `customercategories`
 CHANGE COLUMN `CustomerCategoryID` `CustomerCategoryID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `customertransactions` WRITE;
 ALTER TABLE `customertransactions`
 CHANGE COLUMN `CustomerTransactionID` `CustomerTransactionID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `deliverymethods` WRITE;
 ALTER TABLE `deliverymethods`
 CHANGE COLUMN `DeliveryMethodID` `DeliveryMethodID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `invoicelines` WRITE;
 ALTER TABLE `invoicelines`
 CHANGE COLUMN `InvoiceLineID` `InvoiceLineID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `invoices` WRITE;
 ALTER TABLE `invoices`
 CHANGE COLUMN `InvoiceID` `InvoiceID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `packagetypes` WRITE;
 ALTER TABLE `packagetypes`
 CHANGE COLUMN `PackageTypeID` `PackageTypeID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `paymentmethods` WRITE;
 ALTER TABLE `paymentmethods`
 CHANGE COLUMN `PaymentMethodID` `PaymentMethodID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `purchaseorderlines` WRITE;
 ALTER TABLE `purchaseorderlines`
 CHANGE COLUMN `PurchaseOrderLineID` `PurchaseOrderLineID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `purchaseorders` WRITE;
 ALTER TABLE `purchaseorders`
 CHANGE COLUMN `PurchaseOrderID` `PurchaseOrderID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `specialdeals` WRITE;
 ALTER TABLE `specialdeals`
 CHANGE COLUMN `SpecialDealID` `SpecialDealID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `stateprovinces` WRITE;
 ALTER TABLE `stateprovinces`
 CHANGE COLUMN `StateProvinceID` `StateProvinceID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `stockgroups` WRITE;
 ALTER TABLE `stockgroups`
 CHANGE COLUMN `StockGroupID` `StockGroupID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `stockitemholdings` WRITE;
 ALTER TABLE `stockitemholdings`
 CHANGE COLUMN `StockItemID` `StockItemID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `stockitemimages` WRITE;
 ALTER TABLE `stockitemimages`
 CHANGE COLUMN `StockItemID` `StockItemID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `stockitems` WRITE;
 ALTER TABLE `stockitems`
 CHANGE COLUMN `StockItemID` `StockItemID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `stockitemstockgroups` WRITE;
 ALTER TABLE `stockitemstockgroups`
 CHANGE COLUMN `StockItemStockGroupID` `StockItemStockGroupID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `stockitemtransactions` WRITE;
 ALTER TABLE `stockitemtransactions`
 CHANGE COLUMN `StockItemTransactionID` `StockItemTransactionID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `suppliercategories` WRITE;
 ALTER TABLE `suppliercategories`
 CHANGE COLUMN `SupplierCategoryID` `SupplierCategoryID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `suppliers` WRITE;
 ALTER TABLE `suppliers`
 CHANGE COLUMN `SupplierID` `SupplierID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `suppliertransactions` WRITE;
 ALTER TABLE `suppliertransactions`
 CHANGE COLUMN `SupplierTransactionID` `SupplierTransactionID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `systemparameters` WRITE;
 ALTER TABLE `systemparameters`
 CHANGE COLUMN `SystemParameterID` `SystemParameterID` INT(11) NOT NULL AUTO_INCREMENT;
 
+LOCK TABLES `transactiontypes` WRITE;
 ALTER TABLE `transactiontypes`
 CHANGE COLUMN `TransactionTypeID` `TransactionTypeID` INT(11) NOT NULL AUTO_INCREMENT;
 
 SET FOREIGN_KEY_CHECKS = 1;
+UNLOCK TABLES;
 
 /**
     Triggers.
@@ -105,6 +133,7 @@ SET FOREIGN_KEY_CHECKS = 1;
 #Trigger on people insert, check if email is valid
 
 DELIMITER //
+DROP TRIGGER IF EXISTS correcte_email;
 CREATE TRIGGER correcte_email
     BEFORE INSERT ON people
     FOR EACH ROW
@@ -119,6 +148,7 @@ DELIMITER ;
 #Trigger on customer insert, check if postalcode is valid
 
 DELIMITER //
+DROP TRIGGER IF EXISTS correcte_postcode;
 CREATE TRIGGER correcte_postcode
     BEFORE INSERT ON customers
     FOR EACH ROW
@@ -177,11 +207,11 @@ DELIMITER ;
 /**
   Authorisation.
  */
-DROP USER'nerdygadgets_read'@'localhost';
-DROP USER'nerdygadgets_create'@'localhost';
-DROP USER'nerdygadgets_update'@'localhost';
-DROP USER'nerdygadgets_create_or_update'@'localhost';
-DROP USER'nerdygadgets_delete'@'localhost';
+DROP USER IF EXISTS'nerdygadgets_read'@'localhost';
+DROP USER IF EXISTS'nerdygadgets_create'@'localhost';
+DROP USER IF EXISTS'nerdygadgets_update'@'localhost';
+DROP USER IF EXISTS'nerdygadgets_create_or_update'@'localhost';
+DROP USER IF EXISTS'nerdygadgets_delete'@'localhost';
 
 CREATE USER 'nerdygadgets_read'@'localhost' IDENTIFIED BY '^jnx$PK&hHg3Cz6y#V#S';
 REVOKE ALL PRIVILEGES ON * . * FROM 'nerdygadgets_read'@'localhost';
