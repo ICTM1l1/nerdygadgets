6ea6f9222952d648bc2ce004a2a65d2092354078
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2020-12-12 20:07:29 +0100

    Hide order history for admin users

--- Src/Html/account-navbar.php
+++ Src/Html/account-navbar.php
@@ -4,9 +4,11 @@
             <li class="nav-item <?= strpos(get_current_url(), 'account') !== false ? 'active' : '' ?>">
                 <a class="nav-link" href="<?= get_url('account.php') ?>">Account</a>
             </li>
+            <?php if (!authorizeAdmin()) : ?>
             <li class="nav-item <?= strpos(get_current_url(), 'orders') !== false ? 'active' : '' ?>">
                 <a class="nav-link" href="<?= get_url('orders.php') ?>">Bestelgeschiedenis</a>
             </li>
+            <?php endif; ?>
             <?php if (authorizeAdmin()) : ?>
                 <li class="nav-item <?= strpos(get_current_url(), 'contact-requests') !== false ? 'active' : '' ?>">
                     <a class="nav-link" href="<?= get_url('contact-requests.php') ?>">Contact aanvragen</a>
