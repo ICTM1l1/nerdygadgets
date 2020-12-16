6eeedf68c5858971e3b90848831a2e5dd78efd41
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2020-12-16 14:07:06 +0100

    Refactor very dumb way of creating the table

--- database-additions.sql
+++ database-additions.sql
@@ -17,14 +17,12 @@ CREATE TABLE `nerdygadgets`.`privatecustomer` (
 
 DROP TABLE IF EXISTS `nerdygadgets`.`contact_requests`;
 CREATE TABLE `nerdygadgets`.`contact_requests` (
-   `ContactRequestID` INT NOT NULL AUTO_INCREMENT,
+   `ContactRequestID` INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT,
    `ContactRequestName` VARCHAR(100) NOT NULL,
    `ContactRequestEmail` VARCHAR(100) NOT NULL,
    `ContactRequestSubject` VARCHAR(100) NOT NULL,
    `ContactRequestMessage` TEXT(2000) NOT NULL,
-   `ContactRequestDate` DATETIME NOT NULL DEFAULT (CURRENT_TIMESTAMP),
-   PRIMARY KEY (`ContactRequestID`),
-   UNIQUE INDEX `idcontact_requests_UNIQUE` (`ContactRequestID` ASC)
+   `ContactRequestDate` DATETIME NOT NULL DEFAULT (CURRENT_TIMESTAMP)
 );
 
 /** Password for this user is 'nimda' */
