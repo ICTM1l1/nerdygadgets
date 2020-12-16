d6eadec961035aba16271379e965d62094a4f3d7
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2020-12-16 08:38:40 +0100

    Refactor api key to config

--- Config/config.php
+++ Config/config.php
@@ -37,4 +37,6 @@ $config['debug'] = true;
 $config['session_lifetime'] = 3600;
 $config['csrf_token_lifetime'] = 3600;
 
+$config['temperature_api_key'] = '55ed7846125b1aa3abc20c2c430133cc17e9a61a9f2b906dc15ee7c0179eacdc4b102927ea9e1e84c359d35e7dfe0a4fade8e6de5ba8c5c0c072fc98a293e473';
+
 return $config;
--- Public/temperatureupdate.php
+++ Public/temperatureupdate.php
@@ -1,20 +1,20 @@
 <?php
 require_once __DIR__ . "/../Src/header.php";
-$apiKey = "55ed7846125b1aa3abc20c2c430133cc17e9a61a9f2b906dc15ee7c0179eacdc4b102927ea9e1e84c359d35e7dfe0a4fade8e6de5ba8c5c0c072fc98a293e473";
+$apiKey = config_get('temperature_api_key');
 
-if (isset($_POST["ApiKey"])) {
+if (isset($_POST["ApiKey"]) && isset($_POST["Temperature"])) {
     if ($_POST["ApiKey"] == $apiKey) {
         $checkDB = getTemperature();
         if (!empty($checkDB)) {
             update("coldroomtemperatures", [
-                "Temperature" => (int)$_POST["Temperature"]
+                "Temperature" => (int) $_POST["Temperature"]
             ], [
                 "ColdRoomTemperatureID" => 1
             ]);
         }
         else {
             insert("coldroomtemperatures", [
-                "Temperature" => (int)$_POST["Temperature"],
+                "Temperature" => (int) $_POST["Temperature"],
                 "ColdRoomSensorNumber" => 1,
                 "RecordedWhen" => date("Y-m-d G:i:s"),
                 "ValidFrom" => date("Y-m-d G:i:s"),
