a8d8aaf6e1e06e709c40932d434b0463036dbe81
ikbenlike <thewatcheriswatchinu@gmail.com>
2020-12-20 15:36:42 +0100

    update temperatureupdate.php for consistency

--- Public/temperatureupdate.php
+++ Public/temperatureupdate.php
@@ -1,7 +1,7 @@
 <?php
-require_once __DIR__ . "/../Src/header.php";
+require_once __DIR__ . '/../Src/header.php';
 $apiKey = config_get('temperature_api_key');
 
-if (isset($_POST["ApiKey"], $_POST["Temperature"]) && $_POST["ApiKey"] === $apiKey) {
+if (isset($_POST['ApiKey'], $_POST['Temperature']) && $_POST['ApiKey'] === $apiKey) {
     createOrUpdateTemperatureMeasurement($_POST['Temperature'] ?? 0);
 }
